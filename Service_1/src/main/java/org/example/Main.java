359f6428941f92c18eb2cb567d51cef0a34750df
divyanshu gaur <divyanshugaur14@gmail.com>
2025-03-29 01:00:39 +0530

    merge into main - pqr

--- Service_1/src/main/java/org/example/Main.java
+++ Service_1/src/main/java/org/example/Main.java
@@ -4,5 +4,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world from Service 1");
         System.out.println("yeah buddy - rebase!");
+        System.out.println("yeah buddy - main merge from service1 in pqr branch");
     }
 }
