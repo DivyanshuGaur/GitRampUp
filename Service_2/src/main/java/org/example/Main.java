5d129d21d026c30149d9c9e298731a00076dac46
divyanshu gaur <divyanshugaur14@gmail.com>
2025-03-29 19:15:34 +0530

    adding new func2 in service 2

--- Service_2/src/main/java/org/example/Main.java
+++ Service_2/src/main/java/org/example/Main.java
@@ -7,5 +7,8 @@ public class Main {
     public void func1(){
         System.out.println("Committing func1 in service 2");
     }
+    public void func2(){
+        System.out.println("Patch - test from feat-abc to feat-xyz : Service 2 ");
+    }
 
 }
